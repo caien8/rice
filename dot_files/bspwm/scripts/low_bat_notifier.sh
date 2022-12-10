43d58917b9c2337042bb894e8e0e2ae1e79c7a15
caien <caien811_P@proton.me>
2022-12-10 18:06:59 +0545

    modified configs

--- dot_files/bspwm/scripts/low_bat_notifier.sh
+++ dot_files/bspwm/scripts/low_bat_notifier.sh
@@ -2,14 +2,14 @@
 
 ### VARIABLES
 
-POLL_INTERVAL=120    # seconds at which to check battery level
-LOW_BAT=33           # lesser than this is considered low battery
+POLL_INTERVAL=10    # seconds at which to check battery level
+LOW_BAT=10         # lesser than this is considered low battery
 
 # If BAT0 doesn't work for you, check available devices with command below
 #
 #   $ ls -1 /sys/class/power_supply/
 #
-BAT_PATH=/sys/class/power_supply/BAT0
+BAT_PATH=/sys/class/power_supply/BATT
 BAT_STAT=$BAT_PATH/status
 
 if [[ -f $BAT_PATH/charge_full ]]
